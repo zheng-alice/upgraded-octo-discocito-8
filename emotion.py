0052ccef39e3b1de55d8d9fc9d6f8dfde8e65b2f
Christina Xiao <xiao.christina@gmail.com>
2018-08-05 04:19:29 -0400

    emotion rec works

--- emotion.py
+++ emotion.py
@@ -59,20 +59,14 @@ def record_voice():
 @ask.intent("SearchIntent")
 def search_intent(query):
 	with open("stats.pkl", mode="rb") as f:
-		all_stats = pickle.load(f)
+		stats = pickle.load(f)
 
 	voice_sent = sentiment(query, path = 'sentiment_analysis/sentnet.dat')
 
 	img = take_picture()
 	img = img.copy(order='C')
 	img = np.array(img)
-	names, face_sent_str = face.faceRec2('/Users/christinaxiao/Christina/COLLEGE/Camps/MIT BWSI/Class/Week 2 - Visual/upgraded-octo-discocito-3/dataface/dataface.dat', img)
-	name = names[0]
-
-	if name not in all_stats:
-		return question("Please say 'Peanuts' and then your name to log your name in the database.").reprompt("Please say 'Peanuts' and then your name to log your name in the database.")
-	else:
-		stats = all_stats[name]
+	face_sent_str = face.faceRec2('/Users/christinaxiao/Christina/COLLEGE/Camps/MIT BWSI/Class/Week 2 - Visual/upgraded-octo-discocito-3/dataface/dataface.dat', img)
 	
 	if face_sent_str == 'sad':
 		face_sent = 0
@@ -85,67 +79,44 @@ def search_intent(query):
 
 	stats[sent] += 1
 	with open("stats.pkl", mode="wb") as f:
-		pickle.dump(all_stats, f)
+		pickle.dump(stats, f)
 
     # sad
 	if sent == 0:
-		sad = ["I'm sorry to hear that, {}. ".format(name),
-		"Oh no, I'm sorry to hear that, {}. ".format(name),
-		"I sorry to hear you’re having a hard time, {}. ".format(name),
-		"Oh no, {}. ".format(name)]
+		sad = ["I'm sorry to hear that. ",
+		"Oh no, I'm sorry to hear that. ",
+		"I sorry to hear you’re having a hard time. ",
+		"Oh no. "]
 
 		# very sad
 		if stats[0] + stats[1] >= 5 and stats[0] / (stats[0] + stats[1]) >= 0.8:
-			recs = ["You seem to be really sad lately. I recommend you talk to me by saying 'Alexa talk to me,' ask for reassurance by saying 'Alexa reassure me,' or meditate with me by saying 'Alexa let's meditate.'",
-			"You seem to be really sad lately. I think you should talk to me by saying 'Alexa talk to me,' ask for reassurance by saying 'Alexa reassure me,' or meditate with me by saying 'Alexa let's meditate.'",
-			"You seem to be really sad lately. Try talking to me by saying 'Alexa talk to me,' asking for reassurance by saying 'Alexa reassure me,' or meditating with me by saying 'Alexa let's meditate.'",
-			"You seem to be really sad lately. Why don't you talk to me by saying 'Alexa talk to me,' ask for reassurance by saying 'Alexa reassure me,' or meditate with me by saying 'Alexa let's meditate?'",]
+			recs = ["You seem to be really sad lately. I recommend you talk to me by saying 'Alexa let's talk,' or meditate with me by saying 'Alexa let's meditate.'",
+			"You seem to be really sad lately. I think you should talk to me by saying 'Alexa let's talk,' or meditate with me by saying 'Alexa let's meditate.'",
+			"You seem to be really sad lately. Try talking to me by saying 'Alexa let's talk,' or meditating with me by saying 'Alexa let's meditate.'",
+			"You seem to be really sad lately. Why don't you talk to me by saying 'Alexa let's talk,' or meditate with me by saying 'Alexa let's meditate?'"]
 		# kinda sad
 		else: 
-			recs = ["I recommend you meditate with me by saying 'Alexa let's meditate,' or listen to one of my poems by saying 'Alexa read me a poem.' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
-			"I think you should meditate with me by saying 'Alexa let's meditate,' or listen to one of my poems by saying 'Alexa read me a poem.' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
-			"Try meditating with me by saying 'Alexa let's meditate,' or listening to one of my poems by saying 'Alexa read me a poem.' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
-			"Why don't you meditate with me by saying 'Alexa let's meditate,' or listen to one of my poems by saying 'Alexa read me a poem'? If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes."]
+			recs = ["I recommend you meditate with me by saying 'Alexa let's meditate,' or listen to a short story by saying 'Alexa sad story.' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
+			"I think you should meditate with me by saying 'Alexa let's meditate,' or listen to a short story by saying 'Alexa sad story.' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
+			"Try meditating with me by saying 'Alexa let's meditate,' or listening to a short story by saying 'Alexa sad story.'' If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes.",
+			"Why don't you meditate with me by saying 'Alexa let's meditate,' or listen to a short story by saying 'Alexa sad story.'? If you want me to cheer you up, you can also say 'Alexa Despacito time' to play Despacito, or 'Alexa jokebot' to listen to one of my jokes."]
 
 		return statement(sad[np.random.randint(len(sad))] + recs[np.random.randint(len(recs))])
 	# happy
 	else:
-		happy = ["That's great, {}! ".format(name),
-		"I'm so happy for you, {}! ".format(name),
-		"I'm glad you're doing well, {}. ".format(name),
-		"Sounds exciting, {}! ".format(name),
-		"Sounds great, {}! ".format(name),
-		"Cool, {}! ".format(name)]
+		happy = ["That's great! ",
+		"I'm so happy for you! ",
+		"I'm glad you're doing well. ",
+		"Sounds exciting! ",
+		"Sounds great! ",
+		"Cool! "]
 
-		recs = ["I think you should listen to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a poem by saying, 'Alexa read me a poem.' I can also compliment you if you say 'Alexa give me a compliment'!",
-		"Try listening to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a poem by saying, 'Alexa read me a poem.' I can also compliment you if you say 'Alexa give me a compliment'!",
-		"Why don't you listen to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a poem by saying, 'Alexa read me a poem'? I can also compliment you if you say 'Alexa give me a compliment'!"]
+		recs = ["I think you should listen to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a short story by saying, 'Alexa happy story.' I can also compliment you if you say 'Alexa compliment me'!",
+		"Try listening to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a short story by saying, 'Alexa happy story.' I can also compliment you if you say 'Alexa compliment me'!",
+		"Why don't you listen to a joke by saying 'Alexa jokebot,' a meme by saying 'Alexa memebot,' or a short story by saying, 'Alexa happy story.'? I can also compliment you if you say 'Alexa compliment me'!"]
 
 		return statement(happy[np.random.randint(len(happy))] + recs[np.random.randint(len(recs))])
 	
-@ask.intent("NameIntent")
-def load_new(name):
-	with open("stats.pkl", mode="rb") as f:
-		all_stats = pickle.load(f)
-
-	all_stats[name] = [0, 0]
-
-	with open("stats.pkl", mode="wb") as f:
-		pickle.dump(all_stats, f)
-
-	questions = ["How was your day?",
-	"What's up?",
-	"How's it going?",
-	"How have you been?",
-	"How do you feel?",
-	"What's crackalackin?",
-	"How do you do?", 
-	"What's popping?",
-	"How are you doing?"]
-
-	q = "You have been logged in the database. " + questions[np.random.randint(len(questions))]
-	return question(q).reprompt(q)
-
 @ask.intent("AMAZON.FallbackIntent")
 def no_query():
 	return question("I'm sorry, but I didn't understand that. Try again, this time prefacing your statement with my name, Peanuts.")
